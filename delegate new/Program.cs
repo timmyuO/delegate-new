604fc68e44a0565db2eefb95a1afc2a1714bf046
timmyuO <111019271+timmyuO@users.noreply.github.com>
2022-08-16 03:40:22 -0700

    Add project files.

---
+++ delegate new/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate2
+{
+    internal class Program
+    {
+
+        public delegate bool Predicate(XClass x);
+
+        void LambdaExpression()
+        {
+            var likelyUsername = "timmy";
+
+            Predicate classwork = (XClass x) =>
+            {
+                if (x.UserName == likelyUsername)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+        }
+    }
+    class XClass
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+
+
+}
